df90d806d52260f155f5d63fb21e90b9d63784f5
Emil Berger <emil@Emils-MacBook-Pro.local>
2018-04-28 11:55:37 +0200

    Add Folders for Xcode and NetBeans

---
+++ Zweiradpruefstand_AVR/Xcode/Zweiradpruefstand_AVR/app.c
@@ -0,0 +1,47 @@
+#include "global.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "app.h"
+#include "mon.h"
+#include "sys.h"
+
+// defines
+// ...
+
+
+// declarations and definations
+
+volatile struct App app;
+
+
+// functions
+
+void app_init (void) {
+    memset((void *)&app, 0, sizeof(app));
+}
+
+
+//--------------------------------------------------------
+
+void app_main (void) {
+    
+}
+
+//--------------------------------------------------------
+
+void app_task_1ms (void) {}
+void app_task_2ms (void) {}
+void app_task_4ms (void) {}
+void app_task_8ms (void) {}
+void app_task_16ms (void) {}
+void app_task_32ms (void) {}
+void app_task_64ms (void) {}
+void app_task_128ms (void) {}
+
+
