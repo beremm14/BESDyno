c318626bcb8ea387495222ad5a86ac6f8ee5d492
Emil Berger <emil@Emils-MacBook-Pro.local>
2018-04-29 10:51:37 +0200

    Test ADCs

--- Zweiradpruefstand_AVR/Xcode/Zweiradpruefstand_AVR/t_app.c
+++ Zweiradpruefstand_AVR/Xcode/Zweiradpruefstand_AVR/t_app.c
@@ -17,38 +17,47 @@
 
 // declarations and definations
 
+enum State {MUXVCC, MSTARTVCC, MUXTEMP, MSTARTTEMP};
+enum State state = MUXVCC;
+
 volatile struct App app;
 
 
 // functions
 
-void adc_init (void) {
-    ADMUX = (1<<REFS0);    //select AVCC as reference
-    ADSRA = (1<<ADEN) | 7; //enable and prescale = 128 (16MHz/128 = 125kHz)
-}
-
-int readAdc (char chan) {
-    ADMUX = (1<<REFS0) | (chan & 0x0f); //select input and ref
-    ADSRA |= (1<<ADSC);                 //start the conversation
-    while (ADSRA & (1<<ADSC));          //wait for the end
-    return ADCW;
-}
-
 
 void app_init (void) {
     memset((void *)&app, 0, sizeof(app));
+    
+    ADMUX  = (1<<REFS0) | (1<<ADLAR) | 0x0e;
+    //REFS0 setzt den Referenzwert auf die Versorgungsspannung
+    //ADLAR lässt den Wert im ADC links ausrichten
+    //0x0e setzt die Bandgap-Reference auf 1.1V, Multiplexerkanal
+    
+    ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+    //ADEN enabled den ADC
+    //Die weiteren 3 Bits setzen den Prescaler auf 128
+    //ADC-Frequenz = Arduino-Frequenz/Prescaler
 }
 
 
 //--------------------------------------------------------
 
 void app_main (void) {
+    float vcc = (1.1 * 256 * 0.9784735812) / ADCH; //In Klammer, damit nicht einzeln gerechnet
+    //256: weil 8-Bit-Wert
+    //Korrekturwert, gemessen, real (5V/5.11V)
+    int vk = (int)vcc;
+    int nk = (int)((vcc- (float)vk) * 100.0);
     
+    printf("ADC = 0x%02x VCC = %d.%02d         \n", ADCH, vk, nk);
 }
 
 //--------------------------------------------------------
 
-void app_task_1ms (void) {}
+void app_task_1ms (void) {
+    ADCSRA |= (1<<ADSC);
+}
 void app_task_2ms (void) {}
 void app_task_4ms (void) {}
 void app_task_8ms (void) {}
@@ -56,6 +65,5 @@ void app_task_16ms (void) {}
 void app_task_32ms (void) {}
 void app_task_64ms (void) {}
 void app_task_128ms (void) {}
-
 
 
