71bf3e0473ad971d15640ae0b95340805202be1c
Emil Berger <beremm14@htlkaindorf.at>
2018-11-09 08:38:03 +0100

    Remove: embedXcode-Project

--- Zweiradpruefstand_Arduino/Zweiradpruefstand_Arduino.ino.ino
+++ Zweiradpruefstand_Arduino/Zweiradpruefstand_Arduino.ino.ino
@@ -1,36 +1,3 @@
-//
-// Zweiradpruefstand_Arduino
-//
-// Description of the project
-// Developed with [embedXcode](http://embedXcode.weebly.com)
-//
-// Author 		Emil Berger
-// 				DA-Team (BES)
-//
-// Date			15.10.18 15:34
-// Version		<#version#>
-//
-// Copyright	© Emil Berger, 2018
-// Licence		<#licence#>
-//
-// See         ReadMe.txt for references
-//
-
-
-// Core library for code-sense - IDE-based
-// !!! Help: http://bit.ly/2AdU7cu
-#if defined(ENERGIA) // LaunchPad specific
-#include "Energia.h"
-#elif defined(TEENSYDUINO) // Teensy specific
-#include "Arduino.h"
-#elif defined(ESP8266) // ESP8266 specific
-#include "Arduino.h"
-#elif defined(ARDUINO) // Arduino 1.8 specific
-#include "Arduino.h"
-#else // error
-#error Platform not defined
-#endif // end IDE
-
 #include <Adafruit_BMP085.h>
 #include <Wire.h>
 
