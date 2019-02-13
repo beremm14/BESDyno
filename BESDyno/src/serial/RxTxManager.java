88423801f969d7a4a48a7b24bf4dc5577250d436
Emil Berger <beremm14@htlkaindorf.at>
2019-02-13 13:00:14 +0100

    Add: RxTxManager - should optimize communication for macOS

---
+++ BESDyno/src/serial/RxTxManager.java
@@ -0,0 +1,16 @@
+package serial;
+
+import gnu.io.SerialPort;
+
+/**
+ *
+ * @author emil
+ */
+public class RxTxManager {
+    
+    boolean mac;
+    jssc.SerialPort jssc;
+    SerialPort rxtx;
+    
+
+}
